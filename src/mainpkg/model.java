6c7604391733b5a265b858b5cb4d3cdd65e6aee6
Ryan Trackenberg <ryantrack@yahoo.com>
2019-04-08 13:52:25 -0400

    added attributes and methods to model

--- src/mainpkg/model.java
+++ src/mainpkg/model.java
@@ -1,5 +1,32 @@
 package mainpkg;
 
+import java.util.*;
 public class model {
+	int frameWidth;
+	int frameHeight;
+	int score;
+	List<Obstacle> obstacles;
+	List<Objective> objectives;
+	
+	
+	public void Collision(GameObject g1, GameObject g2){
+		
+	}
+	
+	public void updateGameState(){
+		
+	}
+	
+	public void updateLocation(){
+		
+	}
+	
+	public void superPower(){
+		
+	}
+	
+	public void main(String[] args){
+		
+	}
 
 }
