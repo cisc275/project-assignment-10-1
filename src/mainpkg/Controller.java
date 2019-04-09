f53d6b479f501d768be3d20526b53d6d8ff49a5a
Ryan Trackenberg <ryantrack@yahoo.com>
2019-04-08 21:51:38 -0400

    implemented controller interfaces

--- src/mainpkg/Controller.java
+++ src/mainpkg/Controller.java
@@ -1,6 +1,12 @@
 package mainpkg;
 
-public class Controller {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.*;
+
+public class Controller implements KeyListener, ActionListener{
 	private Model Model;
 	private View View;
 	
@@ -10,5 +16,22 @@ public class Controller {
 	private void main(String[] args){
 		
 	}
+	
+	public void keyPressed(KeyEvent e){
+		
+	}
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
