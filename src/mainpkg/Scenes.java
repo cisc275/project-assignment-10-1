8cc8aef5579ce294aefd941002a33b40b7b23920
Christoffer Braun <braunc@udel.edu>
2019-04-08 21:18:42 -0400

    added scenes enum

---
+++ src/mainpkg/Scenes.java
@@ -0,0 +1,14 @@
+package mainpkg;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public enum Scenes {
+	SCENE1,SCENE2,SCENE3;
+	
+	ArrayList<BufferedImage> gameOneImages = new ArrayList<BufferedImage>();
+	ArrayList<BufferedImage> gameTwoImages = new ArrayList<BufferedImage>();
+	ArrayList<BufferedImage> gameThreeImages = new ArrayList<BufferedImage>();
+	
+}
+
+
