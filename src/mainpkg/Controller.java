fcf3af9a1bc73d29747853af4658e2e114328d02
Christoffer Braun <braunc@udel.edu>
2019-04-08 21:24:22 -0400

    slight modifies to existing, added contents to gameObject

--- src/mainpkg/Controller.java
+++ src/mainpkg/Controller.java
@@ -1,6 +1,11 @@
 package mainpkg;
 
-public class Controller {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Controller implements KeyListener,ActionListener {
 	private Model Model;
 	private View View;
 	
@@ -8,6 +13,22 @@ public class Controller {
 		
 	}
 	private void main(String[] args){
+		
+	}
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 
--- src/mainpkg/GameObject.java
+++ src/mainpkg/GameObject.java
@@ -1,5 +1,18 @@
 package mainpkg;
 
-public class GameObject {
-
+public abstract class GameObject {
+	private int width;
+	private int height;
+	private int xloc;
+	private int yloc;
+	private int xvel;
+	private int yvel;
+	
+	public void move() {
+		
+	}
+	
+	void updateScore() {
+		
+	}
 }
