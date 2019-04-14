4a0f96efdbc976500dac97e24d958bcc14320b5f
clarifiedButter <wca827@gmail.com>
2019-04-14 16:34:43 -0400

    added key class

---
+++ src/mainpkg/Key.java
@@ -0,0 +1,15 @@
+package mainpkg;
+
+public class Key {
+	
+	public boolean isDown;
+	public static Key enter = new Key();
+	public static Key up = new Key();
+	public static Key left = new Key();
+	public static Key right = new Key();
+	public static Key down = new Key();
+	
+	public void toggle() {
+		isDown = !isDown;
+	}
+}
