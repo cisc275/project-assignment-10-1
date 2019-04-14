44c09c80079867e380ec2429f9093848ed8c1b66
Ryan Trackenberg <ryantrack@yahoo.com>
2019-04-14 12:05:58 -0400

    added constructor to view class

--- src/mainpkg/View.java
+++ src/mainpkg/View.java
@@ -1,16 +1,30 @@
 package mainpkg;
 
 
+import java.awt.Color;
 import java.util.*;
 
-public class View {
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+
+
+public class View extends JPanel{
 	private int frameWidth;
 	private int frameHeight;
 	private int frameCount;
+	static JFrame frame;
 	
 	private int picNum;
 	private List<GameObject> gameObjects;
 	
+	public View(){
+		frame = new JFrame();
+    	frame.getContentPane().add(this);
+    	frame.setBackground(Color.gray);
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	frame.setSize(frameWidth, frameHeight);
+    	frame.setVisible(true);
+	}
 	
 	public void loadImages(){
 		
