8ff9685f91f8c58b2ee99353fb6930eba3e2b4f9
clarifiedButter <wca827@gmail.com>
2019-04-14 13:28:21 -0400

    fixed untracked files

--- src/mainpkg/Controller.java
+++ src/mainpkg/Controller.java
@@ -4,14 +4,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-<<<<<<< HEAD
+
 import java.util.*;
 
-public class Controller implements KeyListener, ActionListener{
-=======
 
 public class Controller implements KeyListener,ActionListener {
->>>>>>> 9063243a18bd59f483ac53cc039a2fb4c988c17c
+
 	private Model Model;
 	private View View;
 	
@@ -21,8 +19,7 @@ public class Controller implements KeyListener,ActionListener {
 	private void main(String[] args){
 		
 	}
-<<<<<<< HEAD
-	
+
 	public void keyPressed(KeyEvent e){
 		
 	}
@@ -39,23 +36,4 @@ public class Controller implements KeyListener,ActionListener {
 		
 	}
 	
-=======
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
->>>>>>> 9063243a18bd59f483ac53cc039a2fb4c988c17c
-
 }
--- src/mainpkg/GameObject.java
+++ src/mainpkg/GameObject.java
@@ -1,6 +1,5 @@
 package mainpkg;
 
-<<<<<<< HEAD
 public abstract class GameObject {
 	private int width;
 	private int height;
@@ -16,20 +15,5 @@ public abstract class GameObject {
 	void updateScore() {
 		
 	}
-=======
-public class GameObject {
- private int width;
- private int height;
- private int xloc;
- private int yloc;
- private int xvel;
- private int yvel;
- 
- public void move(){
-	 
- }
- public void updateScore(){
-	 
- }
->>>>>>> ccd0490f9b700a0c2a013960702e1c9410a5607c
+
 }
