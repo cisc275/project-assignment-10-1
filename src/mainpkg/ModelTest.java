6a3cd89a90040375cdb743025be49b6402e915e7
Ryan Trackenberg <ryantrack@yahoo.com>
2019-04-15 13:35:57 -0400

    Fourth verse same as the first

--- src/mainpkg/ModelTest.java
+++ src/mainpkg/ModelTest.java
@@ -13,14 +13,18 @@ public class ModelTest {
 		int fh = 325;
 		
 		Model myModel = new Model(fw, fh, 0);
-		GameObject myObj = new Obstacle(100, 100, 250, 200, 0, 0);
+		GameObject myObj1 = new Obstacle(100, 100, 250, 200, 0, 0);
 		GameObject myObj2 = new Obstacle(100,100,250,0,0,0);
 		GameObject myObj3 = new Obstacle(100,100,250,325,0,0);
+		GameObject myObj4 = new Objective(100,100,0,200,0,0,false,0);
+		GameObject myObj5 = new Objective(100,100,500,200,0,0,false,0);
 		GameObject player = new Player(100,100,250,200,0,0,0);
-		assertFalse("shouldn't collide with anything", myModel.wallCollision(myObj));
+		assertFalse("shouldn't collide with anything", myModel.wallCollision(myObj1));
 		assertTrue("Should collide with wall",myModel.wallCollision(myObj2));
 		assertTrue("Should collide with wall", myModel.wallCollision(myObj3));
-		assertTrue("Collision should occur",myModel.collision(player, myObj));
+		assertTrue("Should collide with wall", myModel.wallCollision(myObj4));
+		assertTrue("Should collide with wall", myModel.wallCollision(myObj5));
+		assertTrue("Collision should occur",myModel.collision(player, myObj1));
 		
 	}
 
