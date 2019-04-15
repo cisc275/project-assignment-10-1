524a87624735ec5af84cca807755a1f6bb406a11
Ryan Trackenberg <ryantrack@yahoo.com>
2019-04-15 13:21:44 -0400

    added a collision test

--- src/mainpkg/ModelTest.java
+++ src/mainpkg/ModelTest.java
@@ -16,6 +16,8 @@ public class ModelTest {
 		GameObject myObj = new Obstacle(100, 100, 250, 200, 0, 0);
 		
 		assertFalse("shouldn't collide with anything", myModel.wallCollision(myObj));
+		GameObject player = new Player(100,100,250,200,0,0,0);
+		assertTrue("Collision should occur",myModel.collision(player, myObj));
 		
 	}
 
