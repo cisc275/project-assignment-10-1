c86b18c57a45ecc5be2f12fe66e1bf87e2de432d
Trevor Seidel <seidelt@udel.edu>
2019-04-15 13:12:06 -0400

    Example test

--- src/mainpkg/ModelTest.java
+++ src/mainpkg/ModelTest.java
@@ -8,7 +8,15 @@ public class ModelTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+		int fw = 500;
+		int fh = 325;
+		
+		Model myModel = new Model(fw, fh, 0);
+		GameObject myObj = new Obstacle(100, 100, 250, 200, 0, 0);
+		
+		assertFalse("shouldn't collide with anything", myModel.wallCollision(myObj));
+		
 	}
 
 }
