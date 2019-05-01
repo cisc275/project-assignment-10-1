8f7bdb4ad5e8323db1f2422adb5725665d6038f7
mvermont <mvermont@udel.edu>
2019-05-01 09:46:12 -0400

    fixing merge errors

--- src/mainpkg/BirdSelectorView.java
+++ src/mainpkg/BirdSelectorView.java
@@ -6,12 +6,10 @@ import javax.swing.JTextArea;
 
 public class BirdSelectorView extends View {
 
-	
-
 	public void startBirdSelection() {
 		//	frame.requestFocus();
-			frame.setVisible(true);
 			JTextArea title = new JTextArea("Select a bird. Press enter to proceed");
 			frame.add(title, BorderLayout.PAGE_START);
+			frame.setVisible(true);
 		}
 }
